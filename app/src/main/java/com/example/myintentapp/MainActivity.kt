978ff87a715b31780c7cc1876b9bd682fd3d2b7b
Tyjoong <csalma251@gmail.com>
2024-01-19 14:37:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.myintentapp
+
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
+        btnDialPhone.setOnClickListener(this)
+
+        val btnBiodata : Button = findViewById(R.id.btn_my_data)
+        btnBiodata.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data -> {
+                val moveWithDataIntent =
+                    Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Tynotrch")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 17)
+                startActivity(moveWithDataIntent)
+            }
+            R.id.btn_my_data -> {
+                val bbio =
+                    Intent(this@MainActivity, MoveWithBiodataActivity::class.java)
+                bbio.putExtra(MoveWithBiodataActivity.EXTRA_NAME, "Tynotrch")
+                bbio.putExtra(MoveWithBiodataActivity.EXTRA_AGE, 17)
+                bbio.putExtra(MoveWithBiodataActivity.EXTRA_ALAMAT, "malang")
+                bbio.putExtra(MoveWithBiodataActivity.EXTRA_SCH, "SMK Telkom Malang")
+                startActivity(bbio)
+            }
+            R.id.btn_dial_number -> {
+                val phoneNumber = "081210841382"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL,
+                    Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
+            }
+        }
+    }
+}
